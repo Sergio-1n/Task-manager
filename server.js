c8a68f69ae7eb7882ba50bba9bc631473fea7e40
Sergio-1n <skuznetcov150@gmail.com>
2024-11-29 23:26:56 +0100

    add express with body-parser

--- server.js
+++ server.js
@@ -1,2 +1,24 @@
-const sum = (a, b) => a + b;
-console.log(sum(10, 5));
+import express from 'express';
+import bodyParser from 'body-parser';
+import swaggerUI from 'swagger-ui-express';
+import swaggerSpec from './config/swagger.js';
+import './config/db.js';
+
+// Routes
+import authRouter from './routes/authRoutes.js';
+import taskRouter from './routes/taskRoutes.js';
+
+const app = express();
+const port = 3001;
+
+//Middleware
+app.use(bodyParser.json());
+
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+app.use('/api', authRouter);
+app.use('/api', taskRouter);
+
+app.listen(port, () => {
+  console.log(`Server listening  at http://localhost:${port}`);
+});
